013ea1984ebf98d9ad98675a25044325f6bdd5ac
Bridget Asser <bridgetasser@gmail.com>
2020-01-24 17:01:46 -0500

    Contact view added

--- job_tracker/views.py
+++ job_tracker/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Contact, Landmark
+
+class ContactCreate(CreateView):
+    model = Contact
+    fields = ['name', 'email', 'linkedin', 'notes', 'application']
+    success = '/home/'
+
+class ContactUpdate(UpdateView):
+    model = Contact
+    fields = ['name', 'email', 'linkedin', 'notes', 'application']
+    success = '/home/'
+
+class ContactDelete(DeleteView):
+    model = Contact
+    fields = ['name', 'email', 'linkedin', 'notes', 'application']
+    success ='/home/'
 
 def home(request):
-    return HttpResponse('This is the home page')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
