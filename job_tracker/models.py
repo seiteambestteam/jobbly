a66d37f1f05886135634ef83193758d7d11378f0
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-01-24 12:16:29 -0500

    updated models based on team feedback, added user model

--- job_tracker/models.py
+++ job_tracker/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from datetime import date, datetime
+from django.contrib.auth.models import AbstractUser
 
-class JobUser(models.Model):
-    #extend the user model to inlude links
-    #extends user - michelle will figure out how to do this
+class User(AbstractUser):
     linkedin = models.CharField(max_length=200)
     github = models.CharField(max_length=200)
     website = models.CharField(max_length=200)
@@ -19,18 +18,18 @@ class Application(models.Model):
     applicationDate = models.DateField()
     dueDate = models.DateField()
     notes = models.TextField(max_length=500)
-    user = models.ForeignKey(JobUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Contact(models.Model):
     name = models.CharField(max_length=200)
-    email = models.CharField(max_length=50)
-    linkedin = models.CharField(max_length=50)
-    notes = models.TextField(max_length=300)
+    email = models.CharField(max_length=100)
+    linkedin = models.CharField(max_length=100)
+    notes = models.TextField(max_length=400)
     application = models.ForeignKey(Application, blank=True, null=True, on_delete=models.SET_NULL)
-    user = models.ForeignKey(JobUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Landmark(models.Model):
-    #event is a python keyword, so ive gone with landmark. we can change this if yall want
+    #drop down
     name = models.CharField(max_length=50)
     datetime = models.DateTimeField()
     location = models.CharField(max_length=200)
