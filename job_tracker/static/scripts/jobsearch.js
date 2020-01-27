1e85a5c8b38cd34f057a53efe7605d79e0e8e00b
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-01-27 14:40:53 -0500

    Update jobsearch.js
    fixed gits interpretation of the jobsearch.js file

--- job_tracker/static/scripts/jobsearch.js
+++ job_tracker/static/scripts/jobsearch.js
@@ -1,26 +1,53 @@
-$(".search-btn").click(() => {
-    const searchTerm = $("#search-term").val();
-    searchString = searchTerm.replace(/\s+/g, "+").toLowerCase();
-    const searchUrl = `https://www.simplyhired.ca/search?q=${searchString}`;
+//api
+$('.search-btn').click(()=>{
+    const searchTerm = $('#search-term').val()
     $.ajax({
-        url: "/ajax/jobsearch/",
+        url: '/ajax/job_search_api/',
         data: {
-            url: searchUrl
+            'searchTerm' : searchTerm,
         },
-        dataType: "json",
+        dataType: 'json',
         success: function(data) {
-            for (i = 0; i < data.length; i++) {
+             for (i = 0; i < data.length; i++) {
                 const card = `<div class='card'>
-                    <p class=''>${data[i].title} at ${data[i].company} in ${data[i].location}</p>
-                    <p class="txt-sml">${data[i].description}</p>
-                    <button class='btn btn-md center txt-center'><a href='${data[i].job_link}'>More!</a></button>
+                    <p class=''>${data.jobs[i].title} at ${data.jobs[i].company} in ${data.jobs[i].location}</p>
+                    <p class="txt-sml">${data.jobs[i].description}</p>
+                    <button class='btn btn-md center txt-center'><a href='${data.jobs[i].url}'>More!</a></button>
                 </div>`;
                 $("#scrapper-results").append(card);
             }
         },
         error: function(err) {
-            console.log(err);
             $("#scrapper-results").append("<p>Error, please try again</p>");
         }
-    });
-});
+    })
+})
+            
+
+//web scraper
+// $('.search-btn').click(()=>{
+//     const searchTerm = $('#search-term').val()
+//     searchString = searchTerm.replace(/\s+/g, '+').toLowerCase()
+//     const searchUrl = `https://www.simplyhired.ca/search?q=${searchString}`
+//     $.ajax({
+//         url: '/ajax/jobsearch/',
+//         data: {
+//             'url' : searchUrl
+//         },
+//         dataType: 'json',
+//         success: function(data) {
+//             for (i = 0; i< data.length; i++) {
+//                 const card= `<div class='card'>
+//                     <span class='job-title'>${data[i].title} at ${data[i].company} in ${data[i].location}</span>
+//                     <p>${data[i].description}</p>
+//                     <a href='${data[i].job_link}' class='btn'>Learn more!</a>
+//                 </div>`
+//                 $('#scrapper-results').append(card)
+//             }
+//         },
+//         error: function(err) {
+//             console.log(err)
+//             $('#scrapper-results').append('<p>Error, please try again</p>')
+//         }
+//     })
+// })
