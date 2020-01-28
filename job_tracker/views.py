9399ed1ea5c0a21a4f9c86bacac14d308d3633d5
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-01-28 17:36:33 -0500

    application view

--- job_tracker/views.py
+++ job_tracker/views.py
@@ -129,7 +129,8 @@ def calendar(request):
 
 def contacts_index(request):
     contacts = Contact.objects.filter(user=request.user)
-    return render(request, 'contacts/index.html', { 'contacts': contacts})
+    all_contacts = contacts.order_by('name')
+    return render(request, 'contacts/index.html', { 'contacts': all_contacts})
 
 def contacts_detail(request, contact_id):
     contact = Contact.objects.get(id=contact_id)
