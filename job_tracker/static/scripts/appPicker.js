809c1edfa9cc5959ff2dec53c5133d3fbb8f9432
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-01-29 17:18:01 -0500

    made materialize dark blue

--- job_tracker/static/scripts/appPicker.js
+++
@@ -1,7 +0,0 @@
-const dateEl = document.getElementById('id_dueDate');
-  M.Datepicker.init(dateEl, {
-    format: 'yyyy-mm-dd',
-    defaultDate: new Date(),
-    setDefaultDate: true,
-    autoClose: true
-  });
