2e1264156b4d3acdcd775da87edb126f2ea802f5
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-01-30 15:48:06 -0500

    removed scraper

--- job_tracker/static/scripts/jobsearch.js
+++ job_tracker/static/scripts/jobsearch.js
@@ -44,31 +44,3 @@ $(".search-btn").click(() => {
         }
     });
 });
-
-//web scraper
-// $('.search-btn').click(()=>{
-//     const searchTerm = $('#search-term').val()
-//     searchString = searchTerm.replace(/\s+/g, '+').toLowerCase()
-//     const searchUrl = `https://www.simplyhired.ca/search?q=${searchString}`
-//     $.ajax({
-//         url: '/ajax/jobsearch/',
-//         data: {
-//             'url' : searchUrl
-//         },
-//         dataType: 'json',
-//         success: function(data) {
-//             for (i = 0; i< data.length; i++) {
-//                 const card= `<div class='card'>
-//                     <span class='job-title'>${data[i].title} at ${data[i].company} in ${data[i].location}</span>
-//                     <p>${data[i].description}</p>
-//                     <a href='${data[i].job_link}' class='btn'>Learn more!</a>
-//                 </div>`
-//                 $('#scrapper-results').append(card)
-//             }
-//         },
-//         error: function(err) {
-//             console.log(err)
-//             $('#scrapper-results').append('<p>Error, please try again</p>')
-//         }
-//     })
-// })
