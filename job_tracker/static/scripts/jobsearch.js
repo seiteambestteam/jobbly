91fc58586548c23607cfe91374b6b8ce8f44f3d8
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-01-30 13:35:28 -0500

    put buttons on same line

--- job_tracker/static/scripts/jobsearch.js
+++ job_tracker/static/scripts/jobsearch.js
@@ -24,14 +24,16 @@ $(".search-btn").click(() => {
                     const card = `<div>
                         <p class="txt-center">${data.jobs[i].title} at ${data.jobs[i].company} in ${data.jobs[i].locations}</p>
                         <p class="txt-sml">${data.jobs[i].description}</p>
-                        <button class='btn center txt-center'><a href='${data.jobs[i].url}' target="_blank">More!</a></button>
-                        <form method='POST' action='${JOBBLY_APPLICATION_LINK}' style='display:inline;'>
-                            <input type="hidden" name="csrfmiddlewaretoken" value="${JOBBLY_CSRF_TOKEN}">
-                            <input name='jobtitle' type='hidden' value='${data.jobs[i].title}'>
-                            <input name='company' type='hidden' value='${data.jobs[i].company}'>
-                            <input name='joblisting' type='hidden' value='${data.jobs[i].url}'>
-                            <input type='submit' style='display:inline;' value='Add Application' class='btn center txt-center link-btn'>
-                        </form>
+                        <div class='d-flex justify-content-around btn-container'>
+                            <button class='btn'><a href='${data.jobs[i].url}' target="_blank">Job Posting</a></button>
+                            <form method='POST' action='${JOBBLY_APPLICATION_LINK}' style='display:inline;'>
+                                <input type="hidden" name="csrfmiddlewaretoken" value="${JOBBLY_CSRF_TOKEN}">
+                                <input name='jobtitle' type='hidden' value='${data.jobs[i].title}'>
+                                <input name='company' type='hidden' value='${data.jobs[i].company}'>
+                                <input name='joblisting' type='hidden' value='${data.jobs[i].url}'>
+                                <input type='submit' style='display:inline;' value='Add Application' class='btn center txt-center link-btn'>
+                            </form>
+                        <div>
                     </div>`;
                     $("#scrapper-results").append(card);
                 }
