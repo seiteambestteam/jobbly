30d06117f2d7585097c08ab657a6f653f6adda0a
Suzy <suzy.nakayama@gmail.com>
2020-01-31 12:00:57 -0500

    Fixed contact deleting and updating after search

--- job_tracker/static/scripts/contactSearch.js
+++ job_tracker/static/scripts/contactSearch.js
@@ -13,6 +13,10 @@ $(document).ready(function() {
     });
 
     $("#contacts-search").on("input", function(e) {
+        const CONTACT_DELETE_LINK = document.getElementById("contact_delete")
+            .value;
+        const CONTACT_UPDATE_LINK = document.getElementById("contact_update")
+            .value;
         const search = $(this).val();
         const re = new RegExp(search, "i");
         const filteredContacts = contacts
@@ -24,7 +28,10 @@ $(document).ready(function() {
                 <div class="col-sm-6 col-md-4">
                     <div class="card contacts-card m-3">
                         <div class="card-title">
-                            <a href="{% url 'contacts_delete' contact.id%}" class="ml-2"><i class="far fa-times-circle"></i></a>
+                            <a href="${CONTACT_DELETE_LINK}" class="ml-2"><i class="far fa-times-circle"></i></a>
+                            <a href="${CONTACT_UPDATE_LINK}" class="ml-2">
+                            <i class="far fa-edit"></i>
+                        </a>
                             <h5 class="txt-bold txt-center pt-3">${contact.name}</h5>
                         </div>
                         <div class="card-content pl-3 pr-3">`;
@@ -32,7 +39,7 @@ $(document).ready(function() {
                 newHtml += `<h6 class="txt-medium d-inline">Application: ${contact.application} at ${contact.company}</h6>
                             <br>`;
             }
-            if (contact.email != 'None') {
+            if (contact.email != "None") {
                 newHtml += `<h6 class=" txt-medium d-inline"><a href='mailto:${contact.email}'>${contact.email}</a></h6>`;
             }
             if (contact.phone_number) {
