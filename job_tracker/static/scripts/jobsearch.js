8c9f72a5cab4572e1e7224877bbd0ed87859d86a
Suzy <suzy.nakayama@gmail.com>
2020-02-01 17:43:15 -0500

    fixing css 1

--- job_tracker/static/scripts/jobsearch.js
+++ job_tracker/static/scripts/jobsearch.js
@@ -2,9 +2,12 @@
 $(".search-btn").click(() => {
     const searchTerm = $("#search-term").val();
     const locationTerm = $("#search-location").val();
-    const JOBBLY_CSRF_TOKEN = document.getElementById('csrf_token').value;
-    const JOBBLY_APPLICATION_LINK = document.getElementById('application_link').value;
-    $("#scrapper-loader").append('<i class="fas fa-spinner fa-pulse fa-5x"></i>')
+    const JOBBLY_CSRF_TOKEN = document.getElementById("csrf_token").value;
+    const JOBBLY_APPLICATION_LINK = document.getElementById("application_link")
+        .value;
+    $("#scrapper-loader").append(
+        '<i class="fas fa-spinner fa-pulse fa-5x"></i>'
+    );
     $.ajax({
         url: "/ajax/job_search_api/",
         data: {
@@ -22,9 +25,9 @@ $(".search-btn").click(() => {
                 $("#scrapper-results").empty();
                 for (i = 0; i < data.jobs.length; i++) {
                     const card = `<div class='search-result'>
-                        <p class="txt-center">${data.jobs[i].title} at ${data.jobs[i].company} in ${data.jobs[i].locations}</p>
+                        <p class="text-center">${data.jobs[i].title} at ${data.jobs[i].company} in ${data.jobs[i].locations}</p>
                         <p class="txt-sml">${data.jobs[i].description}</p>
-                        <div class='d-flex justify-content-around btn-container'>
+                        <div class='d-flex justify-content-around'>
                             <button class='btn link-btn'><a href='${data.jobs[i].url}' target="_blank">Job Posting</a></button>
                             <form method='POST' action='${JOBBLY_APPLICATION_LINK}' style='display:inline;'>
                                 <input type="hidden" name="csrfmiddlewaretoken" value="${JOBBLY_CSRF_TOKEN}">
